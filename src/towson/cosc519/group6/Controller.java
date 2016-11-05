f2c8f73a6380da70e083c1bb706e113de698032e
adoyle <adoyle@clearedgeit.com>
2016-11-05 14:53:52 -0400

    Added some things to UI, unfinished, but sort of has an idea going

--- src/towson/cosc519/group6/Controller.java
+++ src/towson/cosc519/group6/Controller.java
@@ -1,4 +1,10 @@
 package towson.cosc519.group6;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void btnClick(ActionEvent actionEvent) {
+        System.out.println("Button click");
+    }
 }
--- src/towson/cosc519/group6/Main.java
+++ src/towson/cosc519/group6/Main.java
@@ -1,6 +1,8 @@
 package towson.cosc519.group6;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,10 +12,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setTitle("CPU Scheduler Simulation");
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+        scene.heightProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("New height: " + newValue);
+        });
+        scene.widthProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("New width: " + newValue);
+        });
     }
 
 
