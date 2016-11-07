efde44ceeae44250a9bfe24cbad25c39f7b0c9cb
bjoynes <blakejoynes@gmail.com>
2016-11-07 11:40:17 -0500

    Added UI component to add processes to Queue
    Right now the processes only get added to the list view.

--- src/towson/cosc519/group6/ui/Controller.java
+++ src/towson/cosc519/group6/ui/Controller.java
@@ -1,10 +1,46 @@
 package towson.cosc519.group6.ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import towson.cosc519.group6.Job;
+import towson.cosc519.group6.schedulers.FirstComeFirstServe;
+import towson.cosc519.group6.schedulers.Scheduler;
+
 
 public class Controller {
+      @FXML private TextField burstField;
+      @FXML private TextField startTimeField;
+      @FXML private ChoiceBox algorithmList;
+      @FXML private ListView queueList;
 
-    public void btnClick(ActionEvent actionEvent) {
-        System.out.println("Button click");
+      private static final ObservableList<String> queueItems = FXCollections.observableArrayList();
+
+       @FXML public void addtoQueueClick(ActionEvent actionEvent) {
+
+           System.out.println("Added to Queue!");
+           String algorithmChoice = algorithmList.getValue().toString();
+           System.out.println(algorithmChoice);
+           String burstTime = burstField.getText();
+           String startTime = startTimeField.getText();
+
+
+           queueItems.add("Burst: " + burstTime + " " + "Start: "+ startTime);
+
+           queueList.setItems(queueItems);
+
+
+       }
+
+
+
+
+       @FXML public void runQueueProcesses(){
+
+
     }
+
+
 }
